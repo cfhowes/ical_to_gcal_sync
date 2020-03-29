e4d54e6db2ad84b891aa18c31df19ca25525905f
cfh <cfhowes@gmail.com>
2020-03-28 20:18:25 -0700

    PEP-8 for auth.py, relative import to support pytest.

--- auth.py
+++ auth.py
@@ -5,14 +5,15 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-from config import CREDENTIAL_PATH, CLIENT_SECRET_FILE, SCOPES
+from .config import CREDENTIAL_PATH, CLIENT_SECRET_FILE, SCOPES
+
 
 def auth_with_calendar_api():
     creds = None
 
     # this file stores your access and refresh tokens, and is
-    # created automatically when the auth flow succeeeds for 
-    # the first time. 
+    # created automatically when the auth flow succeeeds for
+    # the first time.
     if os.path.exists(CREDENTIAL_PATH):
         # if credentials file fails to load (e.g. because it's the old
         # style JSON content instead), just delete it
@@ -26,9 +27,10 @@ def auth_with_calendar_api():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE,
-                                                             [SCOPES])
-            creds = flow.run_console() # or run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRET_FILE,
+                [SCOPES])
+            creds = flow.run_console()  # or run_local_server(port=0)
 
         # save credentials if successful
         with open(CREDENTIAL_PATH, 'wb') as token:
@@ -37,4 +39,3 @@ def auth_with_calendar_api():
     service = build('calendar', 'v3', credentials=creds)
 
     return service
-
